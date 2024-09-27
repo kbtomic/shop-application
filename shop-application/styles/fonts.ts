3ddb5e8e43da2a0432b3af894decf6e59e03837a
“kbtomic” <ktomic01@fesb.hr>
2024-09-27 21:18:44 +0200

    Created a fontStyles file to define and standardize text styles used throughout the application

---
+++ shop-application/styles/fonts.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native';
+import { COLORS } from './colors';
+
+export default StyleSheet.create({
+    regular10Black: {
+        fontSize: 10,
+        color: 'black',
+    },
+    regular12Black: {
+        fontSize: 12,
+        color: 'black',
+    },
+    regular14Black: {
+        fontSize: 14,
+        color: 'black',
+    },
+    bold14Black: {
+        fontSize: 14,
+        color: 'black',
+        fontWeight: 'bold',
+    },
+    regular16Black: {
+        fontSize: 16,
+        color: 'black',
+    },
+    regular12Grey: {
+        fontSize: 12,
+        color: COLORS.lightGrey,
+    },
+    bold16White: {
+        fontSize: 16,
+        color: COLORS.white,
+        fontWeight: 'bold',
+    },	
+});

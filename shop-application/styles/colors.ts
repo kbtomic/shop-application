b6ba0f7b0029bdbb604f453b41658d5abbf69ad4
“kbtomic” <ktomic01@fesb.hr>
2024-09-27 21:18:00 +0200

    Add a colors type definition and a COLORS constant to standardize color values used throughout the application

---
+++ shop-application/styles/colors.ts
@@ -0,0 +1,13 @@
+export type Colors = {
+    purple: string;
+    lightGrey: string;
+    black: string;
+    white: string;
+};
+
+export const COLORS: Colors = {
+    purple: '#9775FA',
+    lightGrey: '#00000066',
+    black: '#000',
+    white: '#fff',
+};

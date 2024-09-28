6426b99a2e3e62c9807a93a8c41daf40ce87fa78
“kbtomic” <ktomic01@fesb.hr>
2024-09-28 09:06:45 +0200

    Added safe area view for IOS

--- shop-application/App.tsx
+++ shop-application/App.tsx
@@ -1,22 +1,24 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Products from './screens/Products';
 import { RootStackParamList } from './types/Navigation';
 import ProductDetailView from './screens/ProductDetailView';
-import fontStyles from './styles/fonts';
+import { COLORS } from './styles/colors';
 
 const RootStack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-        <RootStack.Navigator initialRouteName='Products'>
-          <RootStack.Screen name='Products' component={Products} options={{ headerShown: false }}/>
-          <RootStack.Screen name='ProductDetailView' component={ProductDetailView} />
-        </RootStack.Navigator>
-      </View>
+      <SafeAreaView style={styles.safeArea}>
+        <View style={styles.container}>
+          <RootStack.Navigator initialRouteName='Products'>
+            <RootStack.Screen name='Products' component={Products} options={{ headerShown: false }}/>
+            <RootStack.Screen name='ProductDetailView' component={ProductDetailView} />
+          </RootStack.Navigator>
+        </View>
+      </SafeAreaView>
     </NavigationContainer>
   );
 }
@@ -24,6 +26,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  safeArea: {
+    flex: 1,
+    backgroundColor: COLORS.white,
   },
 });

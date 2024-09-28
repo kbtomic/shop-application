8d67a04860f7a0b9bc78404133cda28a04091dd4
“kbtomic” <ktomic01@fesb.hr>
2024-09-28 15:59:21 +0200

    Implemented filtering by brand

---
+++ shop-application/components/products/BrandFilter.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, SectionList } from 'react-native';
+import { COLORS } from '../../styles/colors';
+import fontStyles from '../../styles/fonts';
+import { groupBrandsByLetter } from '../../utils/GroupBrandsByLetter';
+
+interface BrandFilterProps {
+  brands: string[];
+  selectedBrands: string[];
+  onSelectBrand: (brand: string) => void;
+}
+
+const BrandFilter: React.FC<BrandFilterProps> = ({ brands, selectedBrands, onSelectBrand }) => {
+    const sections = groupBrandsByLetter(brands);
+
+    return (
+        <View style={styles.container}>
+        <SectionList
+            sections={sections}
+            keyExtractor={(item, index) => item + index}
+            renderItem={({ item }) => (
+            <TouchableOpacity style={styles.brandButton} onPress={() => onSelectBrand(item)}>
+                <Text style={selectedBrands.includes(item) ? styles.selectedBrandText : {}}>{item}</Text>
+            </TouchableOpacity>
+            )}
+            renderSectionHeader={({ section: { title } }) => (
+            <View style={styles.headerContainer}>
+                <Text style={fontStyles.mediumBlack}>{title}</Text>
+            </View>
+            )}
+            contentContainerStyle={styles.listContent}
+        />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: 10,
+        backgroundColor: COLORS.white,
+        flex: 1,
+        width: '100%',
+    },
+    headerContainer: {
+        backgroundColor: '#f0f0f0',
+        paddingVertical: 5,
+        paddingHorizontal: 20,
+    },
+    brandButton: {
+        backgroundColor: COLORS.white,
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+    },
+    selectedBrandText: {
+        fontWeight: 'bold',
+    },
+    listContent: {
+        paddingBottom: 10,
+    },
+});
+
+export default BrandFilter;
